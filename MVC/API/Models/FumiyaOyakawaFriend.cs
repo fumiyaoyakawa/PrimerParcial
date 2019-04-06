34ecd82b18b0adc8a515ad3a80e9b1a45c6d9d8c
fumiya <fumiya.om@gmail.com>
2019-04-06 12:28:17 -0400

    commit creacion del api con modelos

---
+++ MVC/API/Models/FumiyaOyakawaFriend.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public enum Tipoamigo
+    {
+        Conocido,
+        CompañeroEstudio,
+        ColegaDeTrabajo,
+        Amigo,
+        AmigoDeInfancia,
+        Pariente
+
+    }
+    public class FumiyaOyakawaFriend
+    {
+        [Key]
+        public int FriendId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Tipoamigo Amigo { get; set; }
+        [Required]
+        public string Nickname { get; set; }
+        public int Birthdate { get; set; }
+    }
+}
